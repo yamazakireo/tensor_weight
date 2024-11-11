a5aebac4f74f133564d61635feff04ce6c20567d
kyodaireo <reoyamazaki8181@gmail.com>
2024-11-11 16:42:34 +0900

    first commit

--- common/layers.py
+++ common/layers.py
@@ -66,6 +66,38 @@ class Affine:
         dx = dx.reshape(*self.original_x_shape)  # 入力データの形状に戻す（テンソル対応）
         return dx
 
+class TensorAffine:
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+        
+        self.W_vec = None
+        self.x = None
+        self.original_x_shape = None
+        # 重み・バイアスパラメータの微分
+        self.dW = None
+        self.db = None
+
+    def forward(self, x):
+        # テンソル対応
+        self.original_x_shape = x.shape
+        x = x.reshape(x.shape[0], -1)
+        self.x = x
+
+        W_vec = np.sum(self.W, axis=0)
+        self.W_vec = W_vec
+
+        out = np.dot(self.x, self.W_vec) + self.b
+
+        return out
+
+    def backward(self, dout):
+        dx = np.dot(dout, self.W_vec.T)
+        self.dW = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)
+        
+        dx = dx.reshape(*self.original_x_shape)  # 入力データの形状に戻す（テンソル対応）
+        return dx
 
 class SoftmaxWithLoss:
     def __init__(self):
